621493351df96a00b5591614ad48f412b942b45b
Lirosk <kirill_crysis@bk.ru>
2024-11-01 17:32:57 +0300

    controller, models, validation. 79

---
+++ src/test/java/com/example/tacocloud/HomeControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.tacocloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.example.tacocloud.controllers.HomeController;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(MockMvcResultMatchers.content().string(containsString("Welcome to Taco Cloud")));
+    }
+}
